bda4ff79fe73af9e469fb6ef52a0de070ceda4d3
terence-lee-xin-jin <89919135+lee-xin-jin-terence@users.noreply.github.com>
2025-05-12 21:22:19 +0800

    Update database.php

--- Server-Side/database.php
+++ Server-Side/database.php
@@ -2,6 +2,13 @@
 
 error_reporting(E_ALL);
 
+/*
+
+    Function for connecting to database
+    Authors: Terence Lee Xin Jin
+             Hoon Wei Xiong
+*/
+
 function connect_to_database()
 {
     $host = "localhost";
