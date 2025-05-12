fdf75848d5375fc75083b0a5fbd92bb489766c26
terence-lee-xin-jin <89919135+lee-xin-jin-terence@users.noreply.github.com>
2025-05-12 21:13:09 +0800

    Update login.php

--- Server-Side/login.php
+++ Server-Side/login.php
@@ -1,28 +1,27 @@
 <?php
-	error_reporting(E_ALL);
+    error_reporting(E_ALL);
     require('./database.php');
 
 
     header("Access-Control-Allow-Origin: *");
-   	header("Access-Control-Allow-Headers: *");
+    header("Access-Control-Allow-Headers: *");
 
     $client_request_data = file_get_contents('php://input');
 
     $client_json_obj = json_decode($client_request_data, true);
 
     $username = $client_json_obj["username"];
-	$password = $client_json_obj["password"];
+    $password = $client_json_obj["password"];
 	
     $db_connection = connect_to_database();
 
     $escUsername = mysqli_real_escape_string($db_connection , $username);
-	$escPassword = mysqli_real_escape_string($db_connection , $password);
-	$hashedPassword = hash('sha256', $escPassword );
+    $escPassword = mysqli_real_escape_string($db_connection , $password);
+    $hashedPassword = hash('sha256', $escPassword );
 
     //check if user exists
-    $queryHarBatch = "SELECT ApiaristId
-					FROM APIARIST WHERE
-					ApiaristId = '$escUsername' AND Password = '$hashedPassword';";
+    $queryHarBatch = "SELECT ApiaristId FROM APIARIST WHERE
+			ApiaristId = '$escUsername' AND Password = '$hashedPassword';";
 
     $queryResult = mysqli_query($db_connection,$queryHarBatch);
 
@@ -35,17 +34,17 @@
     {
         mysqli_close($db_connection);
 		
-		$server_reply["loginOutcome"] = "failed";
+	$server_reply["loginOutcome"] = "failed";
         $server_reply["errorMessage"] = "Invalid Credentials Entered";
         echo json_encode($server_reply);
         die;
     }
 	
-	mysqli_close($db_connection);
+    mysqli_close($db_connection);
 
-	$server_reply["loginOutcome"] = "success";
+    $server_reply["loginOutcome"] = "success";
     $server_reply["username"] = $escUsername;
 	
     
-	echo json_encode($server_reply);
+    echo json_encode($server_reply);
 ?>
