7c505777cba9669fb7d09e89f66444299f975bc3
terence-lee-xin-jin <89919135+lee-xin-jin-terence@users.noreply.github.com>
2025-05-12 21:17:50 +0800

    Update add-hive-event-data.php

--- Server-Side/add-hive-event-data.php
+++ Server-Side/add-hive-event-data.php
@@ -2,29 +2,31 @@
     error_reporting(E_ALL);
     require('./database.php');
 
-
+    /*
+    	Allow for users to add hive event data
+    */
     $client_request_data = file_get_contents('php://input');
 
     $client_json_obj = json_decode($client_request_data, true);
 
-	$eventType = $client_json_obj["eventType"];
+    $eventType = $client_json_obj["eventType"];
     $description = $client_json_obj["description"];
     $date = $client_json_obj["date"];
     $weatherCondition = $client_json_obj["weatherCondition"];
-	$apiaristId = $client_json_obj["apiaristId"];
-	$hiveId = $client_json_obj["hiveId"];
+    $apiaristId = $client_json_obj["apiaristId"];
+    $hiveId = $client_json_obj["hiveId"];
 
 	
-	$db_connection = connect_to_database();
+    $db_connection = connect_to_database();
 
 	
 	
-	$escEventType = mysqli_real_escape_string($db_connection , $eventType);
-	$escDescription = mysqli_real_escape_string($db_connection , $description);
-	$escDate = mysqli_real_escape_string($db_connection , $date);
+    $escEventType = mysqli_real_escape_string($db_connection , $eventType);
+    $escDescription = mysqli_real_escape_string($db_connection , $description);
+    $escDate = mysqli_real_escape_string($db_connection , $date);
     $escWeatherCondition = mysqli_real_escape_string($db_connection , $weatherCondition);
-	$escApiaristId = mysqli_real_escape_string($db_connection , $apiaristId);
-	$escHiveId = mysqli_real_escape_string($db_connection , $hiveId);
+    $escApiaristId = mysqli_real_escape_string($db_connection , $apiaristId);
+    $escHiveId = mysqli_real_escape_string($db_connection , $hiveId);
 	
 	
 	$insertDataQuery = "INSERT INTO HIVE_EVENT (EventType, Description, 
@@ -35,21 +37,21 @@
 
     $insertDataResult = mysqli_query($db_connection, $insertDataQuery);
 	
-	$server_reply = array();
+    $server_reply = array();
 	
-	if ($insertDataResult == true)
-	{
-		$server_reply["addOutcome"] = "success";
-		$server_reply["eventId"] = mysqli_insert_id($db_connection);
+    if ($insertDataResult == true)
+    {
+	$server_reply["addOutcome"] = "success";
+	$server_reply["eventId"] = mysqli_insert_id($db_connection);
     }
     else
-	{
-		$server_reply["addOutcome"] = "failure";
-		$server_reply["errorMessage"] = "Failed to add event data";
+    {
+	$server_reply["addOutcome"] = "failure";
+	$server_reply["errorMessage"] = "Failed to add event data";
 		
-	}
+    }
 	
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Headers: *");
-	echo json_encode($server_reply);
+   echo json_encode($server_reply);
 ?>
