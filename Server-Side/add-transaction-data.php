9987c935ea4ff57634337e4c9f32b9eb2e90d5b6
terence-lee-xin-jin <89919135+lee-xin-jin-terence@users.noreply.github.com>
2025-05-12 21:09:31 +0800

    Update add-transaction-data.php

--- Server-Side/add-transaction-data.php
+++ Server-Side/add-transaction-data.php
@@ -1,8 +1,8 @@
 <?php
 
-	/*
-		
-	*/
+    /*
+	Allow for users to add transaction data
+    */
 
     error_reporting(E_ALL);
     require('./database.php');
@@ -12,44 +12,43 @@
 
     $client_json_obj = json_decode($client_request_data, true);
 
-	$transactionType = $client_json_obj["transactionType"];
+    $transactionType = $client_json_obj["transactionType"];
     $description = $client_json_obj["description"];
     $totalAmount = $client_json_obj["totalAmount"];
     $date = $client_json_obj["date"];
-	$apiaristId = $client_json_obj["apiaristId"];
+    $apiaristId = $client_json_obj["apiaristId"];
 
 
     $db_connection = connect_to_database();
 
-	$escTransactionType = mysqli_real_escape_string($db_connection , $transactionType);
-	$escDescription = mysqli_real_escape_string($db_connection , $description);
-	$escTotalAmount = mysqli_real_escape_string($db_connection , $totalAmount);
+    $escTransactionType = mysqli_real_escape_string($db_connection , $transactionType);
+    $escDescription = mysqli_real_escape_string($db_connection , $description);
+    $escTotalAmount = mysqli_real_escape_string($db_connection , $totalAmount);
     $escDate = mysqli_real_escape_string($db_connection , $date);
-	$escApiaristId = mysqli_real_escape_string($db_connection , $apiaristId);
+    $escApiaristId = mysqli_real_escape_string($db_connection , $apiaristId);
 	
 	
-	$insertDataQuery = "INSERT INTO TRANSACTION (TransactionType, Description, 
+    $insertDataQuery = "INSERT INTO TRANSACTION (TransactionType, Description, 
 				TotalAmount, Date, ApiaristId) VALUES ('$escTransactionType', '$escDescription', 
-                $escTotalAmount, STR_TO_DATE( '$escDate', '%Y-%m-%d' ) ,  '$escApiaristId');";
+    $escTotalAmount, STR_TO_DATE( '$escDate', '%Y-%m-%d' ) ,  '$escApiaristId');";
 
 
     $insertDataResult = mysqli_query($db_connection, $insertDataQuery);
 	
-	$server_reply = array();
+    $server_reply = array();
 	
-	if ($insertDataResult == true)
-	{
+    if ($insertDataResult == true)
+    {
 		$server_reply["addOutcome"] = "success";
 		$server_reply["transactionId"] = mysqli_insert_id($db_connection);
     }
     else
-	{
+    {
 		$server_reply["addOutcome"] = "failure";
 		$server_reply["errorMessage"] = "Failed to add transaction record";
-		
-	}
+    }
     
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Headers: *");
-	echo json_encode($server_reply);
+    echo json_encode($server_reply);
 ?>
